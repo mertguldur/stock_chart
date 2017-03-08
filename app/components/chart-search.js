f66f44cb4a99a01e91ae24b0807fd9baff562534
Mert Guldur <mertguldur@gmail.com>
2017-03-07 21:33:07 -0600

    Plot a line chart for each company

--- app/components/chart-search.js
+++ app/components/chart-search.js
@@ -12,7 +12,39 @@ export default Ember.Component.extend({
     handleChartButton(company) {
       let input = this.get('name');
       let action = this.get('fetchQuotes');
-      action(company.id).then((fetchResults) => this.set('quotes', fetchResults));
+      action(company.id).then((fetchResults) => {
+        var data = {
+          labels: [],
+          datasets: []
+        };
+        var dataset = {
+          label: "Average Price",
+          fill: true,
+          lineTension: 0.1,
+          backgroundColor: "rgba(0,0,128,0.4)",
+          borderColor: "rgba(0,0,128,1)",
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: "rgba(0,0,128,1)",
+          pointBackgroundColor: "#fff",
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgba(123,104,238,1)",
+          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 5,
+          pointHitRadius: 10,
+          data: []
+        };
+        fetchResults.forEach(function(quote) {
+          dataset.data.push(quote.get('average'));
+          data.labels.push(quote.get('date'));
+        });
+        data.datasets.push(dataset);
+        this.set('quoteData', data);
+      });
     }
   }
 
--- app/models/quote.js
+++ app/models/quote.js
@@ -2,8 +2,5 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   date: DS.attr('string'),
-  low: DS.attr('string'),
-  high: DS.attr('string'),
-  open: DS.attr('string'),
-  close: DS.attr('string')
+  average: DS.attr('string')
 });
